32d865e7f1488ad139f1ea65a3e0fcffcee76bc7
danievazquez-utp <danie.vazquez@utpuebla.edu.mx>
2023-04-24 10:21:08 -0600

    Creacion de controlador taller

---
+++ application/controllers/Taller.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Taller extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('Taller/index_view');
+	}
+
+	public function primer_vista()
+	{
+		$this->load->view('primer_vista_view');
+	}
+}
