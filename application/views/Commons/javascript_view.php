c678ed154ec5af55113e22b5a8a6a2deb274522a
danievazquez-utp <danie.vazquez@utpuebla.edu.mx>
2023-04-26 11:05:53 -0600

    Hojas de estilo y javascript agregado a Commons

--- application/views/Commons/javascript_view.php
+++ application/views/Commons/javascript_view.php
@@ -4,3 +4,14 @@
 <script src="<?php echo base_url('vendor/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo base_url('vendor/dist/js/adminlte.min.js') ?>"></script>
+
+<!-- Datatable -->
+<script src="<?php echo base_url('vendor/plugins/datatables/jquery.dataTables.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-responsive/js/dataTables.responsive.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-buttons/js/dataTables.buttons.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-buttons/js/buttons.html5.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-buttons/js/buttons.print.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-buttons/js/buttons.colVis.min.js') ?>"></script>
