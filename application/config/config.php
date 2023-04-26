62a1a76d632cd87c5a527bd49724d8e59b4a386f
Ángel Luna <utp0149900@alumno.utpuebla.edu.mx>
2023-04-26 09:20:46 -0600

    Agregado htaccess para eliminar inde.php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/taller_example/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
