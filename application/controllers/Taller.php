4c5afef9445bbab2778d9da9ce53dc8108ec05b2
danievazquez-utp <danie.vazquez@utpuebla.edu.mx>
2023-04-27 11:10:09 -0600

    Registro y recuperacion de registros de talleres agregado

--- application/controllers/Taller.php
+++ application/controllers/Taller.php
@@ -10,6 +10,8 @@ class Taller extends CI_Controller {
 
 		// Agregar helpers y librerias adicionales al controlador
 		$this->load->helper('url');
+		// Agregar modelo
+		$this->load->model('General_model');
 	}
 
 	public function index()
@@ -39,6 +41,12 @@ class Taller extends CI_Controller {
 
 	public function formulario()
 	{
+		$talleres = $this->General_model->get('talleres', array(), array(), '');
+
+		$data 	= array(
+			'talleres'	=>	$talleres,
+		);
+
 		$this->load->view('Commons/head_view');
 		$this->load->view('Commons/body_open_view');
 		$this->load->view('Commons/wrapper_open_view');
@@ -46,7 +54,7 @@ class Taller extends CI_Controller {
 		$this->load->view('Commons/main_sidebar_view');
 
 		// Aquí va el contenido
-		$this->load->view('Taller/form_view');
+		$this->load->view('Taller/form_view', $data);
 		//$this->load->view('Commons/content_wrapper_view');
 		// Aquí agregamos ventana modal
 		$this->load->view('Taller/form_modal_view');
@@ -60,5 +68,26 @@ class Taller extends CI_Controller {
 		$this->load->view('Taller/form_js_view');
 		
 		$this->load->view('Commons/body_close_view');
+	}
+
+	public function guardar_formulario()
+	{
+		foreach ($_POST as $key => $value) {
+			echo "$key = $value <br>";
+		}
+
+		$valores = array(
+			'fecha_reg'	=>	date("Y-m-d H:i:s"),
+			'nombre'	=>	$this->input->post('nombre'),
+			'tipo'		=>	$this->input->post('tipo'),
+			'lugar'		=>	$this->input->post('lugar'),
+			'hora'		=>	$this->input->post('hora'),
+			'fecha'		=>	$this->input->post('fecha'),
+			'email'		=>	$this->input->post('correo'),
+			'cupo'		=>	$this->input->post('cupo'),
+		);
+
+		$this->General_model->set('talleres', $valores);
+		redirect(base_url('formulario'));
 	}
 }
--- application/views/Taller/form_view.php
+++ application/views/Taller/form_view.php
@@ -19,7 +19,7 @@
 
     <!-- Main content -->
     <section class="content">
-      <form action="" method="post">
+      <form action="<?php echo base_url('guardar')  ?>" method="post">
         <div class="card card-success collapsed-card">
           <div class="card-header">
             <h4 class="card-title">
@@ -72,7 +72,7 @@
                 <div class="form-group">
                   <label for="">¿Trabajas?</label>
                   <input type="radio" name="trabaja" id="trabaja_1" required="required"> Si
-                  <input type="radio" name="trabaja" id="trabaja_2" required="required"> No
+                  <input type="radio" name="trabaja" id="trabaja_2" required="required" checked="checked"> No
                 </div>
                 <div class="form-group" id="div_trabaja" style="display:none;">
                   <label for="">¿Dónde trabajas?</label>
@@ -108,56 +108,19 @@
               </tr>
             </thead>
             <tbody class="text-center">
-              <tr>
-                <td>1</td>
-                <td>Taller 1</td>
-                <td>15-05-2023</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Taller 2</td>
-                <td>15-05-2023</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td>Taller 3</td>
-                <td>15-05-2023</td>
-              </tr>
-              <tr>
-                <td>4</td>
-                <td>Taller 4</td>
-                <td>15-05-2023</td>
-              </tr>
-              <tr>
-                <td>5</td>
-                <td>Taller 5</td>
-                <td>15-05-2023</td>
-              </tr>
-              <tr>
-                <td>6</td>
-                <td>Taller 6</td>
-                <td>15-05-2023</td>
-              </tr>
-              <tr>
-                <td>7</td>
-                <td>Taller 7</td>
-                <td>15-05-2023</td>
-              </tr>
-              <tr>
-                <td>8</td>
-                <td>Taller 8</td>
-                <td>15-05-2023</td>
-              </tr>
-              <tr>
-                <td>9</td>
-                <td>Taller 9</td>
-                <td>15-05-2023</td>
-              </tr>
-              <tr>
-                <td>10</td>
-                <td>Taller 10</td>
-                <td>15-05-2023</td>
-              </tr>
+              <?php
+              if ($talleres!=false) { 
+                foreach ($talleres->result() as $taller) {
+                  ?>
+                  <tr>
+                    <td><?php echo $taller->id_taller ?></td>
+                    <td><?php echo $taller->nombre ?></td>
+                    <td><?php echo $taller->fecha ?></td>
+                  </tr>
+                  <?php
+                }
+              }
+              ?>
             </tbody>
             <tfoot></tfoot>
           </table>
