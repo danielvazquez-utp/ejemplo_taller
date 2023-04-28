8a4b0d6f21dd1c4f849ba9ad0ece4ba66974ec77
ArletteJM <107292396+ArletteJM@users.noreply.github.com>
2023-04-27 20:46:47 -0600

    Base de datos/correciones

--- application/controllers/Taller.php
+++ application/controllers/Taller.php
@@ -1,35 +1,44 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Taller extends CI_Controller {
+
 	public function __construct()
 	{
 		// Hereda los métodos de la super clase CI_Controles
 		parent::__construct();
+
 		// Agregar helpers y librerias adicionales al controlador
 		$this->load->helper('url');
+		// Agregar modelo
+		$this->load->model('General_model');
 	}
+
 	public function index()
 	{
 		$this->load->view('Taller/index_view');
 	}
+
 	public function primer_vista()
 	{
 		$this->load->view('primer_vista_view');
 	}
+
     public function agregar_taller()
     {
         $this->load->view('Taller/agregar_taller_view');
     }
+
 	public function plantilla()
 	{
 		$this->load->view('Taller/blank_view');
 	}
+
 	public function personal()
 	{
 		$this->load->view('Taller/personal_view');
 	}
 
-	
 	public function formulario()
 	{
 		$talleres = $this->General_model->get('talleres', array(), array(), '');
