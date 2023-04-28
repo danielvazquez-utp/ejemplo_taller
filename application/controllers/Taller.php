848bfe7c260c0d2e2f043c26e54dd8a4ca8b41df
ArletteJM <107292396+ArletteJM@users.noreply.github.com>
2023-04-27 21:08:43 -0600

    mas

--- application/controllers/Taller.php
+++ application/controllers/Taller.php
@@ -44,7 +44,7 @@ class Taller extends CI_Controller {
 
 		$this->load->view('Commons/head_view');
 		$this->load->view('Commons/body_open_view');
-		$this->load->view('Commons/wraper_open_view');
+		$this->load->view('Commons/wrapper_open_view');
 		$this->load->view('Commons/navbar_view');
 		$this->load->view('Commons/main_sidebar_view');
 
@@ -55,7 +55,7 @@ class Taller extends CI_Controller {
 		$this->load->view('Taller/form_modal_view');
 
 		$this->load->view('Commons/footer_view');
-		$this->load->view('Commons/wraper_close_view');
+		$this->load->view('Commons/wrapper_close_view');
 		$this->load->view('Commons/javascript_view');
 
 		// Aquí mi javascript
