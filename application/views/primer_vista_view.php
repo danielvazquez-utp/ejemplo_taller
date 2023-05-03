b82bcac2a31abe2776032854742d808a6194d394
palomagt05 <gatitogt919@gmail.com>
2023-05-02 22:30:16 -0600

    primera vista

--- application/views/primer_vista_view.php
+++ application/views/primer_vista_view.php
@@ -9,7 +9,7 @@
 <body>
     <h1>Primer vista</h1>
     <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et arcu in dolor consequat vestibulum. Etiam sollicitudin vel ex eu lacinia. Donec quis maximus dolor. Ut interdum gravida massa, vel aliquet tellus. Proin mauris lorem, luctus et molestie sit amet, aliquet sed ligula. Mauris sit amet convallis nisl. Curabitur tempus justo nisl, ac faucibus lectus consequat vitae. Donec rhoncus facilisis nisi, a aliquam tellus placerat sed. Proin convallis dolor nec porta volutpat. Pellentesque semper malesuada tortor, et pharetra eros facilisis ac. Nulla facilisi. Aenean vehicula pretium lectus sit amet consequat. Integer bibendum sapien eu nisl bibendum maximus. Donec venenatis feugiat lacinia.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et arcu in dolor consequat vestibulum. 
     </p>
 </body>
 </html>
