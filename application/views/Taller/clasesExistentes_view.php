ab3cc935ae329239235521170a57ffc2e5df695b
Abigail6B <utp0145723@alumno.utpuebla.edu.mx>
2023-06-29 21:31:52 -0600

    clases existentes en orden

--- application/views/Taller/clasesExistentes_view.php
+++ application/views/Taller/clasesExistentes_view.php
@@ -1,24 +1,37 @@
-<div class="content-wrapper">
-    <?php
-      if($clases!=false){
-        foreach ($clases->result() as $clase){
-            ?>
-            <div class="card card-success" style="width: 15rem; padding: 10px;">
-                <div class="card-header">
-                        <?= $clase->materia ?>
-                </div>
-                <div class="card-body">
-                    <h3 class="text-center"><?= $clase->grado?>° <?=$clase->grupo?></h3>
-                </div>
-                <div class="footer">
-                    <a href="<?=base_url('')?>" class="btn btn-success text-center" style="display:flex; aligne-items: center;">
-                    <i class="fas fa-qrcode"> </i>
-                    Generar código QR
-                </a>
-                </div>
-            </div>
-            <?php
-        }
-      }
-    ?>
+<div class="content-wrapper" style="min-height: 606px;">
+    <section class="content">
+        <div class="row">
+        <?php
+            if($clases!=false){
+                foreach ($clases->result() as $clase){
+                    ?>
+                    
+                    <section class="col-xs-12 col-md-4 col-lg-4">
+                        <div class="small-box bg-success">
+                            <div class="card card-success" >
+                                <div class="card-header">
+                                        <?= $clase->materia ?>
+                                </div>
+                                <div class="card-body">
+                                    <div class="inner">
+                                        <h3 class="text-center" style="color: #000000"><?= $clase->grado?>° <?=$clase->grupo?></h3>
+                                    </div>
+                                    
+                                </div>
+                                <div class="footer">
+                                    <a href="<?=base_url('')?>" class="btn btn-success text-center" style="display:flex; aligne-items: center;">
+                                    <i class="fas fa-qrcode"> </i>
+                                    Generar código QR
+                                </a>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <?php
+                }
+            }
+        ?>
+        </div>
+
+    </section>
 </div>
