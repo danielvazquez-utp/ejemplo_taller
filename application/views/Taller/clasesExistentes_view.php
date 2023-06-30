7945940a11fa553b90dc19cca01691972dd4c9e6
Abigail6B <utp0145723@alumno.utpuebla.edu.mx>
2023-06-29 21:42:39 -0600

    clases existentes en orden nuevo

--- application/views/Taller/clasesExistentes_view.php
+++ application/views/Taller/clasesExistentes_view.php
@@ -1,4 +1,19 @@
 <div class="content-wrapper" style="min-height: 606px;">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Clases registradas</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="<?=base_url('home')?>">Inicio</a></li>
+                <li class="breadcrumb-item active">Clases registradas</li>
+                </ol>
+            </div>
+            </div>
+        </div>
+    </section>
     <section class="content">
         <div class="row">
         <?php
